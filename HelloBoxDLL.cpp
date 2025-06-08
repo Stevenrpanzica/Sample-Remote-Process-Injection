fd81e987ee0c99a674d0f613b4b9e08365718c88
Stevenrpanzica <steven.r.panzica@gmail.com>
2025-06-08 17:11:32 -0400

    Update HelloBoxDLL.cpp

--- HelloBoxDLL.cpp
+++ HelloBoxDLL.cpp
@@ -1,24 +1,9 @@
-#include <windows.h>
+// HelloBoxDLL.cpp
+#include <Windows.h>
 
-// Function executed by the new thread
-DWORD WINAPI ShowMessageBoxThread(LPVOID lpParam) {
-    MessageBoxA(NULL, "Hello World", "Injected", MB_OK | MB_ICONINFORMATION);
-    return 0;
-}
-
-// Entry point for the DLL
-BOOL APIENTRY DllMain(HMODULE hModule,
-                      DWORD  ul_reason_for_call,
-                      LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-        // Create a new thread to safely show MessageBox
-        CreateThread(NULL, 0, ShowMessageBoxThread, NULL, 0, NULL);
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        MessageBoxW(NULL, L"Hello from HelloBoxDLL!", L"HelloBox", MB_OK | MB_ICONINFORMATION);
     }
     return TRUE;
 }
